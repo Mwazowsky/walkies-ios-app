aa074d26c1b596e6b995b82eb27e2ad147d34124
Saifulloh Fadli <saifulloh.fadli@gmail.com>
2024-04-25 17:00:32 +0700

    Initial Commit

---
+++ Walkies/WalkiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalkiesApp.swift
+//  Walkies
+//
+//  Created by Saifulloh Fadli on 25/04/24.
+//
+
+import SwiftUI
+
+@main
+struct WalkiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
